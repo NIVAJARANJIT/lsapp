9aa58d3d3fa8c835f55e38e83aed766cb1f8b8d3
Nivaja Ranjit <numetalxz@gmail.com>
2020-07-16 15:38:00 +0545

    added model relationships

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
 
         <div class="card bg-light p-3">
         <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
-        <small>Written on {{$post->created_at}}</small>
+        <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
         </div>
         
     @endforeach
